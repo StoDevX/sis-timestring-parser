4ec638a450b4ae1a09bb72100088291224bcc7d3
Hawken Rives <hawkrives@gmail.com>
2016-10-04 12:05:38 +0900

    fix index.js file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.checkCoursesForTimeConflicts = require('./lib/checkCoursesForTimeConflicts').default
-exports.checkScheduleForTimeConflicts = require('./lib/checkScheduleForTimeConflicts').default
-exports.convertTimeStringsToOfferings = require('./lib/convertTimeStringsToOfferings').default
-exports.findScheduleTimeConflicts = require('./lib/findScheduleTimeConflicts').default
+exports.checkCoursesForTimeConflicts = require('./lib/check-courses-for-time-conflicts').default
+exports.checkScheduleForTimeConflicts = require('./lib/check-schedule-for-time-conflicts').default
+exports.convertTimeStringsToOfferings = require('./lib/convert-time-strings-to-offerings').default
+exports.findScheduleTimeConflicts = require('./lib/find-schedule-time-conflicts').default
