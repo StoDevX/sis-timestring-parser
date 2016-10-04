20a49340a8d1e250ea0c17fa229e17f3e6d8a51b
Hawken Rives <hawkrives@gmail.com>
2016-10-04 11:56:30 +0900

    make another function in checkCoursesForTimeConflicts

--- src/check-courses-for-time-conflicts.js
+++ src/check-courses-for-time-conflicts.js
@@ -14,14 +14,26 @@ function checkOfferingForTimeConflict(main, alternate) {
 	return false
 }
 
+function checkTwoOfferingsForTimeConflict(mainOffer, altOffer) {
+	return some(
+		mainOffer.times,
+		mainTime =>
+			some(
+				altOffer.times,
+				altTime =>
+					checkOfferingForTimeConflict(mainTime, altTime)))
+}
+
 export default function checkCoursesForTimeConflicts(mainCourse, altCourse) {
 	// Check the offerings from two courses against each other.
 	// Returns *as soon as* two times conflict.
-	return some(mainCourse.offerings, mainOffer =>
-		// Two offerings cannot conflict if they are on different days
-		some(filter(altCourse.offerings, offer => offer.day === mainOffer.day),
-			altOffer =>
-				some(mainOffer.times, mainTime =>
-					some(altOffer.times, altTime =>
-						checkOfferingForTimeConflict(mainTime, altTime)))))
+
+	return some(
+		mainCourse.offerings,
+		mainOffer =>
+			some(
+				// Two offerings cannot conflict if they are on different days
+				filter(altCourse.offerings, offer => offer.day === mainOffer.day),
+				altOffer =>
+					checkTwoOfferingsForTimeConflict(mainOffer, altOffer)))
 }
