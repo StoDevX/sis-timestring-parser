482733b86e45ebaebe7e14ce3122fb8295468e1b
Hawken Rives <hawkrives@gmail.com>
2018-02-11 12:24:18 -0600

    begin splitting convertTimeStringsToOfferings to support two grouping modes

--- src/convert-time-strings-to-offerings.js
+++ src/convert-time-strings-to-offerings.js
@@ -5,7 +5,50 @@ import zip from 'lodash/zip'
 import findDays from './find-days'
 import findTime from './find-time'
 
-export default function convertTimeStringsToOfferings(course) {
+export default function convertTimeStringsToOfferings(course, {groupBy='day'}={}) {
+	// you may group by "day" or by "sis", where "sis" retains the groupings given by SIS
+
+	if (groupBy === 'day') {
+		return convertAndGroupByDay(course)
+	} else if (groupBy === 'sis') {
+		return convertAndGroupLikeSis(course)
+	} else {
+		throw new Error(`"${groupBy}" is not a valid groupBy mode`)
+	}
+}
+
+function convertAndGroupByDay(course) {
+	let offerings = {}
+
+	zip(course.times, course.locations).forEach(([sisTimestring, location]) => {
+		const [daystring, timestring] = sisTimestring.split(' ')
+
+		const days = findDays(daystring)
+		const time = findTime(timestring)
+
+		days.forEach(day => {
+			if (!offerings[day]) {
+				offerings[day] = {}
+			}
+
+			let offering = {
+				day: day,
+				times: [assign({}, time)],
+			}
+
+			if (location) {
+				offering.location = location
+			}
+
+			mergeWith(offerings[day], offering,
+				(a, b) => Array.isArray(a) ? a.concat(b) : undefined)
+		})
+	})
+
+	return values(offerings)
+}
+
+function convertAndGroupLikeSis(course) {
 	let offerings = {}
 
 	zip(course.times, course.locations).forEach(([sisTimestring, location]) => {
